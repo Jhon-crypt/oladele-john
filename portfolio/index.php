e460523f96e27b776a9e642bada168da9c97d7f0
Jhon-crypt <johnoladele690@gmail.com>
2022-06-07 19:42:39 -0700

    Update

--- portfolio/index.php
+++ portfolio/index.php
@@ -115,11 +115,15 @@ class portfolio{
       ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur
       adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
     </p>
+
+  </div>
+
     <h3 class="w3-padding-16 w3-text-light-grey">My Skills</h3>
     <p class="w3-wide">Html</p>
     <div class="w3-white">
       <div class="w3-dark-grey" style="height:28px;width:95%"></div>
     </div>
+
 
     <p class="w3-wide">Css</p>
     <div class="w3-white">
@@ -175,6 +179,10 @@ class portfolio{
     </div>
     
     <br>
+
+    </div>
+
+    </div>
 
   
     
